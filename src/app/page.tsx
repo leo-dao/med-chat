dd0f2862c2a3027112631149d20c71477cc195c1
Leo Dao <leotomdao@gmail.com>
2024-04-19 16:33:12 -0400

    Cleaning up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 'use client'; 
 import Chat from './components/Chat';
-import styled from 'styled-components';
-
 
 export default function Home() {
   return (
-    <div>
       <Chat /> 
-    </div>
   );
 }
